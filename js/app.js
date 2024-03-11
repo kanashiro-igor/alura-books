822c621087672a4b9517d95e34d06354b3077e7b
Igor Sousa <kanashiro.igor@gmail.com>
2024-03-11 16:19:00 -0300

    Swiper update and added card. Small code updates.

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    spaceBetween: 10,
+    slidesPerView: 1.5,
+    centeredSlides: 'true',
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+});
